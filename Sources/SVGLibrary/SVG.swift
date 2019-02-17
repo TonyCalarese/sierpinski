651595fd6c2f299e84f0d1ec06153b1bc0229fbe
TonyCalarese <accalarese@gmail.com>
2019-02-17 13:32:33 -0500

    Certificate

--- Sources/SVGLibrary/SVG.swift
+++ Sources/SVGLibrary/SVG.swift
@@ -1,3 +1,18 @@
+/*
+	Author: Tony Calarese, Sean Eggar
+	Class: CSI-380
+	Assignment: Assignment 4
+	Due Date: February 22, 2019 11:59 PM
+	Certification of Authenticity:
+	We certify that this is entirely our own work, except where we have given
+	fully-documented references to the work of others. We understand the definition
+	and consequences of plagiarism and acknowledge that the assessor of this
+	assignment may, for the purpose of assessing this assignment:
+	- Reproduce this assignment and provide a copy to another member of academic
+	- staff; and/or Communicate a copy of this assignment to a plagiarism checking
+	- service (which may then retain a copy of this assignment on its database for
+	- the purpose of future plagiarism checking)
+*/
 //
 //  SVG.swift
 //  SierpinskiSwift
