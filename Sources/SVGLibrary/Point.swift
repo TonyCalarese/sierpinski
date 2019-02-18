083ad707e615909a2eb9130ec59dc090f0108dd2
TonyCalarese <accalarese@gmail.com>
2019-02-18 11:46:09 -0500

    Attempted so psuedo Code
    Never worked with Swift before and attempting to try and get this running still learning

--- Sources/SVGLibrary/Point.swift
+++ Sources/SVGLibrary/Point.swift
@@ -25,4 +25,19 @@ public typealias Point = (x: Int, y: Int)
 // The midpoint between two Points.
 public func midpoint(_ p1: Point, _ p2: Point) -> Point {
     // YOUR CODE HERE
+	/*
+	// Mid point of two points
+Point mid_point(Point p1, Point p2) {
+    Point mid;
+    mid.x = (p1.x + p2.x) / 2;
+    mid.y = (p1.y + p2.y) / 2;
+    return mid;
+}
+*/
+Point midpoint;
+let midpoint.x = (p1.x + p2.x) /2
+let midpoint.y = (p1.y + p2.y) /2
+
+return midpoint
+
 }
